d8538170a5bd09791fc585510092dbaeb9c11d04
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-08 18:39:30 +0100

    index and service pages

--- beauty_shop/views.py
+++ beauty_shop/views.py
@@ -1,94 +1,41 @@
 from datetime import date
 from django.shortcuts import render
-# from django.http import JsonResponse
 
 from .models import Salon, Category, Service, Master, Client, Feedback, Note
 
 
-def get_salons(request):
+def service(request):
     salons = Salon.objects.all().values()
-    return render(request, 'index.html', context={'places': salons})
-
-
-def get_salon(request, pk):
-    salon = Salon.objects.filter(id=pk).values()
-    return render(request, 'index.html', context={'salon': salon})
-
-
-def get_categories(request):
     categories = Category.objects.all().values()
-    return render(request, 'service.html', context={'categories': categories})
-
-
-def get_category(request, pk):
-    category = Category.objects.filter(id=pk).values()
-    return render(request, 'service.html', context={'category': category})
-
-
-def get_services(request):
+    # category_services = Service.objects.filter(category=pk).values()
     services = Service.objects.all().values()
-    return render(request, 'index.html', context={'services': services})
+    # salon_masters = Master.objects.filter(salon=pk).values()
+    data = {
+        'salons': salons,
+        'categories': categories,
+        # 'category_services': category_services,
+        'services': services,
+    }
+    return render(request, 'service.html', context={'data': data})
 
 
-def get_service(request, pk):
-    service = Service.objects.filter(id=pk).values()
-    return render(request, 'index.html', context={'service': service})
-
-
-def get_masters(request):
+def index(request):
+    salons = Salon.objects.all().values()
+    services = Service.objects.all().values()
     masters = Master.objects.all().values()
-    return render(request, 'index.html', context={'masters': masters})
-
-
-def get_master(request, pk):
-    master = Master.objects.filter(id=pk).values()
-    return render(request, 'index.html', context={'master': master})
-
-
-def get_clients(request):
-    clients = Client.objects.all().values()
-    return render(request, 'service.html', context={'clients': clients})
-
-
-def get_client(request, pk):
-    client = Client.objects.filter(id=pk).values()
-    return render(request, 'service.html', context={'client': client})
-
-
-def get_feedbacks(request):
     feedbacks = Feedback.objects.all().values()
-    return render(request, 'index.html', context={'feedbacks': feedbacks})
+    data = {
+        'salons': salons,
+        'services': services,
+        'masters': masters,
+        'feedbacks': feedbacks,
+    }
+    return render(request, 'index.html', context={'data': data})
 
 
-def get_feedback(request, pk):
-    feedback = Feedback.objects.filter(id=pk).values()
-    return render(request, 'index.html', context={'feedback': feedback})
-
-
-def get_notes(request):
-    notes = Note.objects.all().values()
-    return render(request, 'service.html', context={'notes': notes})
-
-
-def get_note(request, pk):
-    note = Note.objects.filter(id=pk).values()
-    # return JsonResponse({'note': list(note)}, json_dumps_params={'indent': 4, 'ensure_ascii': False})
-    return render(request, 'service.html', context={'note': note})
-
-
-def get_category_services(request, pk):
-    category_services = Service.objects.filter(category=pk).values()
-    return render(request, 'service.html', context={'category_services': category_services})
-
-
-def get_salon_masters(request, pk):
-    salon_masters = Master.objects.filter(salon=pk).values()
-    return render(request, 'service.html', context={'salon_masters': salon_masters})
-
-
-def get_master_slots(request, pk):
-    category_services = Master.objects.filter(id=pk).first()
-    print(category_services)
-    date1 = date.today()
-    print(category_services.notes.filter(date_time_start__day=date1.day))
-    return render(request, 'service.html', context={'category_services': category_services})
+# def get_master_slots(request, pk):
+#     category_services = Master.objects.filter(id=pk).first()
+#     print(category_services)
+#     date1 = date.today()
+#     print(category_services.notes.filter(date_time_start__day=date1.day))
+#     return render(request, 'service.html', context={'category_services': category_services})
--- project/urls.py
+++ project/urls.py
@@ -3,33 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from beauty_shop.views import get_salons, get_salon, get_categories, get_category, get_services, get_service, get_masters, get_master,\
-    get_clients, get_client, get_feedbacks, get_feedback, get_notes, get_note, get_category_services, get_master_slots, get_salon_masters
+from beauty_shop.views import index, service
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('salons/', get_salons, name='salons_list'),
-    path('salons/<pk>', get_salon, name='salon_detail_info'),
-    path('salons/<pk>/masters', get_salon_masters, name='salon_masters_list'),
+    path('index/', index, name='index'),
+    path('service/', service, name='service'),
 
-    path('categories/', get_categories, name='categories_list'),
-    path('categories/<pk>', get_category, name='category_detail_info'),
-    path('categories/<pk>/services', get_category_services, name='category_services_list'),
-
-    path('services/', get_services, name='services_list'),
-    path('services/<pk>', get_service, name='service_detail_info'),
-
-    path('masters/', get_masters, name='masters_list'),
-    path('masters/<pk>', get_master, name='master_detail_info'),
-    path('masters/<pk>/slots', get_master_slots, name='master_slots_info'),
-
-    path('clients/', get_clients, name='clients_list'),
-    path('clients/<pk>', get_client, name='client_detail_info'),
-
-    path('feedbacks/', get_feedbacks, name='feedbacks_list'),
-    path('feedbacks/<pk>', get_feedback, name='feedback_detail_info'),
-
-    path('notes/', get_notes, name='notes_list'),
-    path('notes/<pk>', get_note, name='note_detail_info'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
