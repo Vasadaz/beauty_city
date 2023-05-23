8553903d971a37675ab0db96fd13d8b5093ec862
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-05-23 23:17:59 +0300

    Added models to admin.py

--- beauty_shop/admin.py
+++ beauty_shop/admin.py
@@ -1,3 +1,112 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Category, Client, Feedback, Master, Note, Salon, Service
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+    list_per_page = 20
+
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'address',
+        'preview',
+    )
+    list_per_page = 20
+    readonly_fields = ('preview',)
+
+    @admin.display(description='Превью изображения')
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px;">')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'category',
+        'duration',
+        'price',
+        'image',
+        'image_preview',
+    ]
+    list_display = (
+        'title',
+        'duration',
+        'price',
+        'preview',
+    )
+    list_filter = ('category',)
+    list_per_page = 20
+    # raw_id_fields = ('category',)
+    readonly_fields = ('preview',)
+
+    @admin.display(description='Превью изображения')
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px;">')
+
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'surname',
+        'salon',
+        'preview',
+    )
+    list_per_page = 20
+    readonly_fields = ('preview',)
+
+    @admin.display(description='Превью изображения')
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px;">')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'surname',
+        'phonenumber',
+    )
+    list_per_page = 20
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = (
+        'client',
+        'master',
+        'rating',
+    )
+    list_filter = (
+        'client',
+        'master',
+        'rating',
+    )
+    list_per_page = 20
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'client',
+        'master',
+        'service',
+        'date_time_start',
+    )
+    list_filter = (
+        'client',
+        'master',
+        'service',
+        'salon',
+    )
+    list_per_page = 20
