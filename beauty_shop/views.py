149a12f7a8fd8b3d9e1596ecdd6b0179f0c52ae5
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-05-23 22:08:07 +0300

    Added BAD views

--- beauty_shop/views.py
+++ beauty_shop/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def manager(request):
+    return render(request, 'manager.html')
+
+
+def notes(request):
+    return render(request, 'notes.html')
+
+
+def popup(request):
+    return render(request, 'popup.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def service_finally(request):
+    return render(request, 'service_finally.html')
+
