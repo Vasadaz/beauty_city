969daeedf9f9d4ff15b8154e72770293298ef960
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-05-23 22:08:17 +0300

    Added BAD urls

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,25 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+from beauty_shop.views import (
+    index,
+    manager,
+    notes,
+    popup,
+    service,
+    service_finally,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', index, name='index'),
+    path('manager/', manager, name='manager'),
+    path('notes/', notes, name='notes'),
+    path('popup/', popup, name='popup'),
+    path('service/', service, name='service'),
+    path('service_finally/', service_finally, name='service_finally'),
+
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
