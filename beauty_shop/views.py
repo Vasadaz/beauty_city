5842937714267250d9a96ff18fdbde43452c0764
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-05-24 23:35:11 +0300

    Updated view "index"

--- beauty_shop/views.py
+++ beauty_shop/views.py
@@ -1,8 +1,45 @@
+import requests
+
+from django.conf import settings
 from django.shortcuts import render
+from textwrap import dedent
+
+from .models import (
+    Salon,
+    Category,
+    Service,
+    Master,
+    Client,
+    Feedback,
+    Note,
+)
 
 
 def index(request):
-    return render(request, 'index.html')
+    salons = Salon.objects.all()
+    services = Service.objects.all()
+    masters = Master.objects.all()
+    feedbacks = Feedback.objects.all()
+    data = {
+        'salons': salons,
+        'services': services,
+        'masters': masters,
+        'feedbacks': feedbacks,
+    }
+    if request.method == 'POST':
+        user_name = request.POST.get('fname', '')
+        user_tel = request.POST['tel']
+        order_text = request.POST.get('contactsTextarea', '')
+        msg = f'''
+        Заявка на консультацию:
+        Имя: {user_name}
+        Телефон: {user_tel}
+        Сообщение: {order_text}'''
+        url = f"https://api.telegram.org/bot{settings.TOKEN_TG}/sendmessage"
+        params = {'chat_id': settings.CHAT_ID, 'text': dedent(msg)}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+    return render(request, 'index.html', context=data)
 
 
 def manager(request):
