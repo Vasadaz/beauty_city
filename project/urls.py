9c8986d51eca9e0ac7b5344ddbeb15d2904ac7f2
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-05-29 21:26:06 +0300

    FIXED urls

--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', index, name='index'),
-    path('manager', manager, name='manager'),
-    path('notes', notes, name='notes'),
-    path('popup', popup, name='popup'),
-    path('service', service, name='service'),
-    path('service_finally', service_finally, name='service_finally'),
+    path('manager/', manager, name='manager'),
+    path('notes/', notes, name='notes'),
+    path('popup/', popup, name='popup'),
+    path('service/', service, name='service'),
+    path('service_finally/', service_finally, name='service_finally'),
 
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
