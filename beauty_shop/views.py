d9093eed4ab1810991bd9a4df4eca4d131c69a03
nikolay <vasadaz@ya.ru>
2023-05-30 19:01:50 +0300

    BIG update

--- beauty_shop/views.py
+++ beauty_shop/views.py
@@ -1,13 +1,17 @@
 import locale
-from datetime import datetime, timedelta
-from textwrap import dedent
+import uuid
 
+from datetime import datetime, timedelta
+from pathlib import Path
+from textwrap import dedent
+from urllib.parse import urlparse, urlencode
 import requests
 
 from django.conf import settings
 from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from yookassa import Configuration, Payment
 
 from beauty_shop.models import (
     Salon,
@@ -18,6 +22,8 @@ from beauty_shop.models import (
     Feedback,
     Note,
 )
+from project.settings import YOOMANEY_API_KEY, YOOMANEY_SHOP_ID
+
 
 locale.setlocale(
     category=locale.LC_ALL,
@@ -54,33 +60,70 @@ def index(request):
 
 def manager(request):
     if not request.user.is_superuser:
-        return redirect('https://vasadaz.ru/admin/login/?next=/manager/')
+        previous_url = Path(request.META.get('HTTP_REFERER'))
+        new_url = previous_url.parent / 'admin' / 'login'
+        payload = {'next': '/manager/'}
+        parsed = urlparse(new_url.as_posix())
+        merged_params = urlencode(payload)
+        parsed = parsed._replace(query=merged_params)
+        return_url = parsed.geturl()
+        return redirect(return_url)
     all_note = Note.objects.all()
     current_month_visits = all_note.filter(
         date_time_start__month=datetime.today().month,
     )
     visits_per_year = all_note.filter(
         date_time_start__year=datetime.today().year,
-        payment=True,
+        payment_status=True,
     ).count()
-    monthly_total = sum(
-        [
-            visit.service.price for visit in current_month_visits.filter(
-            payment=True
-        )
-        ]
-    )
-    paid_visits = current_month_visits.filter(payment=True).count()
+    monthly_total = current_month_visits.filter(payment_status=True).aggregate(Sum('price'))
+
+    paid_visits = current_month_visits.filter(payment_status=True).count()
     all_entries_month = all_note.count()
     visits_percentage = paid_visits * 100 // all_entries_month
     data = {
         'current_month_visits': paid_visits,
-        'monthly_total': monthly_total,
+        'monthly_total': monthly_total['price__sum'],
         'visits_per_year': visits_per_year,
         'all_entries_month': all_entries_month,
         'visits_percentage': visits_percentage,
     }
     return render(request, 'manager.html', context=data)
+
+def get_payment_url(previous_url: Path, client_id: int, price: str, note_id: int = None, note_ids: list = None) -> str:
+    Configuration.account_id = YOOMANEY_SHOP_ID
+    Configuration.secret_key = YOOMANEY_API_KEY
+
+    if note_ids:
+        description = 'Заказы ' + ''.join([f' №{id},' for id in note_ids])[:-1]
+        note_id = 0
+    else:
+        description = f'Заказ №{note_id}'
+
+    new_url = previous_url.parent / 'check_payment' / str(client_id)
+    payload = {'note': note_id}
+    parsed = urlparse(new_url.as_posix())
+    merged_params = urlencode(payload)
+    parsed = parsed._replace(query=merged_params)
+    return_url = parsed.geturl()
+
+
+
+    payment = Payment.create({
+        'amount': {
+            'value': price,
+            'currency': 'RUB'
+        },
+        'confirmation': {
+            'type': 'redirect',
+            'return_url': return_url,
+        },
+        'capture': True,
+        'description': description,
+        'test': True,
+    }, uuid.uuid4())
+
+    return payment.confirmation.confirmation_url
 
 
 def note(request):
@@ -92,6 +135,7 @@ def note(request):
         client_name = request.POST.get('client_name')
         client_tel = request.POST.get('client_tel')
         client_comment = request.POST.get('client_comment', '')
+        previous_url = Path(request.META.get('HTTP_REFERER'))
 
         date_time_start = datetime.strptime(request.POST.get('datetime'), '%d %B %Y (%A) %H:%M')
         date_time_end = date_time_start + timedelta(hours=service.duration.hour, minutes=service.duration.minute)
@@ -104,11 +148,9 @@ def note(request):
             phonenumber=client_tel,
         )
 
-
         if created_as:
             client_obj.name = client_name
             client_obj.save()
-
 
         note_obj = Note(
             client=client_obj,
@@ -122,22 +164,60 @@ def note(request):
         )
         note_obj.save()
 
+        note_obj.payment_url = get_payment_url(
+            previous_url=previous_url,
+            client_id=client_obj.id,
+            price=note_obj.price,
+            note_id=note_obj.id,
+        )
+        note_obj.save()
+
         return redirect(reverse('notes', args=[client_obj.id]))
 
 
 def notes(request, pk):
     client_obj = Client.objects.get(id=pk)
 
+    try:
+        price_sum = client_obj.notes.filter(payment_status=False).aggregate(Sum('price'))
+        price_sum = price_sum['price__sum']
+        note_ids = client_obj.notes.filter(payment_status=False).values_list('id', flat=True)
+        previous_url = Path(request.META.get('HTTP_REFERER'))
+        price_sum_url = get_payment_url(
+            previous_url=previous_url,
+            client_id=client_obj.id,
+            price=price_sum,
+            note_ids=note_ids,
+        )
+    except TypeError:
+        price_sum=0
+        price_sum_url='#'
+
     data = {
         'client': client_obj,
-        'sum_price': client_obj.notes.filter(payment_status=False).aggregate(Sum('price'))
+        'price_sum': price_sum,
+        'price_sum_url': price_sum_url,
     }
 
     return render(request, 'notes.html', context=data)
 
 
-def popup(request):
-    return render(request, 'popup.html')
+def check_payment(request, pk):
+    client_obj = Client.objects.get(id=pk)
+    note = request.GET.get('note')
+
+    if note != '0':
+        note_obj = Note.objects.get(id=note)
+        note_obj.payment_url = None
+        note_obj.payment_status = True
+        note_obj.save()
+    else:
+        for note_obj in client_obj.notes.filter(payment_status=False):
+            note_obj.payment_url = None
+            note_obj.payment_status = True
+            note_obj.save()
+
+    return redirect(reverse('notes', args=[client_obj.id]))
 
 
 def service(request):
@@ -188,25 +268,3 @@ def service_finally(request):
 
 
     return render(request, 'service_finally.html', context=data)
-
-
-
-"""
-def service_finally(request, pk):
-    note = Note.objects.get(id=pk)
-    data = {'note': note}
-    if request.method == 'POST':
-        fname = request.POST.get('fname', '')
-        tel = request.POST.get('tel')
-
-        if request.user:
-            contactsTextarea = request.POST.get('contactsTextarea', '')
-        else:
-            client, created = Client.objects.get_or_create(
-                name=fname,
-                phonenumber=tel
-            )
-        note.message = contactsTextarea
-        note.save()
-    return render(request, 'service_finally.html', context=data)
-"""
